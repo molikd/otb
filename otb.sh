7275560d2cb25bc750e802e58c202e5372ed7a48
David Molik <david.molik@usda.gov>
2021-09-22 18:23:19 -0500

    polish spillover to run.nf incorrect, fixed

--- otb.sh
+++ otb.sh
@@ -146,7 +146,7 @@ fi
 [ -f "$R1" ] && RUN+="--readf=\"$R1\" " || error "read pair file one not found, exiting"
 [ -f "$R2" ] && RUN+="--readr=\"$R2\" " || error "read pair file two not found, exiting"
 [ -n "$BUSCO" ] && RUN+="$BUSCO " || state "not running busco"
-[ -n "$POLISHTYPE" ] && RUN+="--polish --$POLISHTYPE " || warn "not polishing, it is recomended that you polish"
+[ -n "$POLISHTYPE" ] && RUN+="--polish --polishtype=\"$POLISHTYPE\" " || warn "not polishing, it is recomended that you polish"
 [ -n "$BUSCO" ] && [ -z "$LINEAGE" ] && error "you want to run BUSCO, but busco lineage not set, exiting"
 [ -n "$LINEAGE" ] && RUN+="--linreage=\"$LINEAGE\" "
 [ -n "$BAM" ] && RUN+="--readbam=\"$BAM\" " || error "bam file(s) not given, exiting"
